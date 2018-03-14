32824a98fb145f57b49a5402e08f1373b36b6f15
Yannick Schaus <habpanel@schaus.net>
2018-03-14 11:12:20 +0100

    Better unsupported language handling & reporting
    Signed-off-by: Yannick Schaus <habpanel@schaus.net>

--- src/main/java/org/openhab/ui/habot/nlp/UnsupportedLanguageException.java
+++ src/main/java/org/openhab/ui/habot/nlp/UnsupportedLanguageException.java
@@ -1,10 +1,22 @@
 package org.openhab.ui.habot.nlp;
 
+import java.util.Locale;
+
 public class UnsupportedLanguageException extends Exception {
+
+    private Locale language;
 
     /**
      *
      */
     private static final long serialVersionUID = -7147837667959343830L;
 
+    public UnsupportedLanguageException(String language) {
+        this.language = Locale.forLanguageTag(language);
+    }
+
+    @Override
+    public String getMessage() {
+        return "Unsupported language: " + language.toString();
+    }
 }
