1b4ae3582b5adc49baf8ca514d54061f08ae3eb3
stritti <stephan@st-strittmatter.name>
2018-03-19 09:37:20 +0100

    use consts

--- src/test/java/org/openhab/ui/habot/test/TrainerDeTest.java
+++ src/test/java/org/openhab/ui/habot/test/TrainerDeTest.java
@@ -80,12 +80,12 @@ public class TrainerDeTest extends AbstractTrainerTest {
 		this.trainer = new IntentTrainer("de", skills, null, "alphanumeric");
 
 		actual = interpret("wie ist der Verlauf der Temperatur der letzten Stunde?");
-		assertEquals("get-history-hourly", actual.getName());
+		assertEquals(Skills.GET_HISTORY_HOURLY, actual.getName());
 		assertEquals(1, actual.getEntities().size());
 		assertEquals("temperatur", actual.getEntities().get("object"));
 
 		actual = interpret("Verlauf der Temperatur der letzten Stunde");
-		assertEquals("get-history-hourly", actual.getName());
+		assertEquals(Skills.GET_HISTORY_HOURLY, actual.getName());
 		assertEquals(1, actual.getEntities().size());
 		assertEquals("temperatur", actual.getEntities().get("object"));
 	}
@@ -97,12 +97,12 @@ public class TrainerDeTest extends AbstractTrainerTest {
 		this.trainer = new IntentTrainer("de", skills, null, "alphanumeric");
 
 		actual = interpret("Verlauf der Temperatur der letzten 24 Stunden");
-		assertEquals("get-history-daily", actual.getName());
+		assertEquals(Skills.GET_HISTORY_DAILY, actual.getName());
 		assertEquals(1, actual.getEntities().size());
 		assertEquals("temperatur", actual.getEntities().get("object"));
 
 		actual = interpret("Verlauf der Luftfeuchtigkeit der letzten 24 Stunden");
-		assertEquals("get-history-daily", actual.getName());
+		assertEquals(Skills.GET_HISTORY_DAILY, actual.getName());
 		assertEquals(1, actual.getEntities().size());
 		assertEquals("luftfeuchtigkeit", actual.getEntities().get("object"));
 
@@ -115,17 +115,17 @@ public class TrainerDeTest extends AbstractTrainerTest {
 		this.trainer = new IntentTrainer("de", skills, null, "alphanumeric");
 
 		actual = interpret("Verlauf der Temperatur der letzten Woche");
-		assertEquals("get-history-weekly", actual.getName());
+		assertEquals(Skills.GET_HISTORY_WEEKLY, actual.getName());
 		assertEquals(1, actual.getEntities().size());
 		assertEquals("temperatur", actual.getEntities().get("object"));
 
 		actual = interpret("Verlauf der Luftfeuchtigkeit der letzten Woche");
-		assertEquals("get-history-weekly", actual.getName());
+		assertEquals(Skills.GET_HISTORY_WEEKLY, actual.getName());
 		assertEquals(1, actual.getEntities().size());
 		assertEquals("luftfeuchtigkeit", actual.getEntities().get("object"));
 
 		actual = interpret("zeige die Wochenübersicht der Temperatur für die Küche");
-		assertEquals("get-history-weekly", actual.getName());
+		assertEquals(Skills.GET_HISTORY_WEEKLY, actual.getName());
 		assertEquals(2, actual.getEntities().size());
 		assertEquals("temperatur", actual.getEntities().get("object"));
 		assertEquals("küche", actual.getEntities().get("location"));
@@ -138,17 +138,17 @@ public class TrainerDeTest extends AbstractTrainerTest {
 		this.trainer = new IntentTrainer("de", skills, null, "alphanumeric");
 
 		actual = interpret("Verlauf der Temperatur des letzten Monats");
-		assertEquals("get-history-monthly", actual.getName());
+		assertEquals(Skills.GET_HISTORY_MONTHLY, actual.getName());
 		assertEquals(1, actual.getEntities().size());
 		assertEquals("temperatur", actual.getEntities().get("object"));
 
 		actual = interpret("Verlauf der Luftfeuchtigkeit des letzten Monats");
-		assertEquals("get-history-monthly", actual.getName());
+		assertEquals(Skills.GET_HISTORY_MONTHLY, actual.getName());
 		assertEquals(1, actual.getEntities().size());
 		assertEquals("luftfeuchtigkeit", actual.getEntities().get("object"));
 
 		actual = interpret("Monatsübersicht der Temperatur für die Küche");
-		assertEquals("get-history-monthly", actual.getName());
+		assertEquals(Skills.GET_HISTORY_MONTHLY, actual.getName());
 		assertEquals(2, actual.getEntities().size());
 		assertEquals("temperatur", actual.getEntities().get("object"));
 		assertEquals("küche", actual.getEntities().get("location"));
@@ -161,19 +161,25 @@ public class TrainerDeTest extends AbstractTrainerTest {
         this.trainer = new IntentTrainer("de", skills, null, "alphanumeric");
 
         actual = interpret("wann hat sich die Temperatur das letzte mal geändert?");
-        assertEquals("get-history-last-changes", actual.getName());
+        assertEquals(Skills.GET_HISTORY_LAST_CHANGES, actual.getName());
         assertEquals(1, actual.getEntities().size());
         assertEquals("temperatur", actual.getEntities().get("object"));
         
         actual = interpret("wann wurde der letzte Alarm ausgelöst?");
-        assertEquals("get-history-last-changes", actual.getName());
+        assertEquals(Skills.GET_HISTORY_LAST_CHANGES, actual.getName());
         assertEquals(1, actual.getEntities().size());
         assertEquals("alarm", actual.getEntities().get("object"));        
               
         actual = interpret("wann wurde der Status vom Licht im Wohnzimmer zuletzt geändert?");
-        assertEquals("get-history-last-changes", actual.getName());
+        assertEquals(Skills.GET_HISTORY_LAST_CHANGES, actual.getName());
         assertEquals(2, actual.getEntities().size());
         assertEquals("wohnzimmer", actual.getEntities().get("location"));
         assertEquals("licht", actual.getEntities().get("object"));
-    }    
+    } 
+    
+    @Test
+    public void testDE_setValue() throws Exception {
+        Intent actual;
+        this.trainer = new IntentTrainer("de", skills, null, "alphanumeric");  	
+    }
 }
